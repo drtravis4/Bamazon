538a13b52986d530e3d29b98da07ab531352b618
Travis Bunn <drtravis4@gmail.com>
2020-03-27 22:41:41 -0500

    fixed problems

--- bamazonCustomers.js
+++ bamazonCustomers.js
@@ -2,8 +2,8 @@ var mysql = require("mysql");
 
 require("console.table");
 
-var inquirer = require("inquirer")
-
+var inquirer = require("inquirer");
+var totalPurchases = 0;
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -24,34 +24,56 @@ function displayProducts() {
   connection.query("select * from products", function(error, results) {
     console.table(results);
 
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "please select an item number",
+          name: "itemID"
+        },
+        {
+          type: "input",
+          message: "how many units to buy?",
+          name: "quantity"
+        }
+      ])
+      .then(function(userInput) {
+        var statement = connection.query(
+          "select * from ?? where ?? = ? ",
+          ["products", "item_id", userInput.itemID],
+          function(error, results) {
+            console.table(results);
 
-         inquirer.prompt([{
-             type:"input",
-             message:"please select an item number",
-             name:"itemID"
-         },{
-             type:"input",
-             message:"how many units to buy?",
-            name:"quantity"
-         }
-        
-        ]).then(function(userInput){
-              var statement =   connection.query("select * from ?? where ?? = ? ",["products","item_id",userInput.itemID],function(error,results){
-                     console.table(results)
+            var getQuantitydb = results[0].stock_quantity;
+            var newquantity = getQuantitydb - userInput.quantity;
 
-                      var getQuantitydb= results[0].stock_quantity
+            if (newquantity >= 0) {
+            
+              totalPurchases =
+                totalPurchases + userInput.quantity * results[0].price;
+              console.log("total purchases: $" + totalPurchases.toFixed(2));
+              connection.query(
+                "update ?? set ?? = ? where ?? = ? ",
+                [
+                  "products",
+                  "stock_quantity",
+                  newquantity,
+                  "item_id",
+                  userInput.itemID
+                ],
+                function(error, results) {
+                  displayProducts();
+                }
+              );
+            } else {
+              console.log("insufficent funds");
+              displayProducts()
+            }
+          
+          }
+        );
 
-                      var newquantity = getQuantitydb - userInput.quantity
-
-                       
-                       connection.query("update ?? set ?? = ? where ?? = ? ",
-                       ["products","stock_quantity",newquantity,"item_id",userInput.itemID],function(error,results){
-                           displayProducts()
-                       })
-
-                 })
-
-                 console.log(statement.sql)
-         })
+        console.log(statement.sql);
+      });
   });
 }
